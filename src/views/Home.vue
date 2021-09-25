2e657f897467bc309bb8903e3d81755702f4b213
gdlyeabkov <gdlyeabkov@gmail.com>
2021-09-25 19:03:48 +0300

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,84 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div style="display: flex;">
+      <div style="width: 350px; margin: 0px 75px;">
+
+      </div>
+      <div style="width: calc(100% - 340px); display: flex; justify-content: space-between;">
+        <p style="text-decoration: underline; text-decoration-color: rgb(255, 150, 0); font-weight: bolder;">Overview</p>
+        <div style="justify-content: space-between; display: flex; width: 250px;">
+          <span style="display: inline;" class="material-icons-outlined">
+            auto_stories
+          </span>
+          <p>Repositories</p>
+          <p>149</p>
+        </div>
+        <p>Projects</p>
+        <p>Packages</p>
+      </div>
+    </div>
+    <hr />
+    <div style="display: flex;">
+      <div style="border-radius: 100%; background-color: rgb(0, 255, 0); width: 350px; height: 300px; margin: 0px 75px;">
+
+      </div>
+      <div style="width: calc(100% - 175px);">
+        <p>Popular repositories</p>
+        <div style="display: flex; width: 100%;">
+          
+          <div style="width: 50%;">
+            <dir style="width: 85%; height: 150px; border: 1px solid rgb(175, 175, 175);">
+
+            </dir>
+            <dir style="width: 85%; height: 150px; border: 1px solid rgb(175, 175, 175);">
+              
+            </dir>
+            <dir style="width: 85%; height: 150px; border: 1px solid rgb(175, 175, 175);">
+              
+            </dir>
+          </div>
+          
+          <div style="width: 50%;">
+            <dir style="width: 85%; height: 150px; border: 1px solid rgb(175, 175, 175);">
+
+            </dir>
+            <dir style="width: 85%; height: 150px; border: 1px solid rgb(175, 175, 175);">
+              
+            </dir>
+            <dir style="width: 85%; height: 150px; border: 1px solid rgb(175, 175, 175);">
+              
+            </dir>
+          </div>
+
+        </div>
+        <p>398 contributions in the last year</p>
+        
+        <div style="border: 1px solid rgb(175, 175, 175); border-radius: 5px; height: 175px; width: 90%;">
+
+        </div>
+      
+        <div style="display: flex; justify-content: space-between; width: 90%;">
+          <p>
+            Contribution activity
+          </p>
+          <button class="btn btn-primary">
+            2021
+          </button>
+        </div>
+        <button class="btn btn-light" style="">Show more activity</button>
+
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
